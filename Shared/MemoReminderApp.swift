d863c7a7eb6a8c12ddfb2463b263f94db320304b
Seyyed Parsa Neshaei <seyyedparsa@outlook.com>
2021-07-07 22:47:42 +0430

    Initial Commit

---
+++ Shared/MemoReminderApp.swift
@@ -0,0 +1,20 @@
+//
+//  MemoReminderApp.swift
+//  Shared
+//
+//  Created by Seyyed Parsa Neshaei on 7/7/21.
+//
+
+import SwiftUI
+
+@main
+struct MemoReminderApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
