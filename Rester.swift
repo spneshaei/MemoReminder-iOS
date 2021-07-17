10b953924ce69d10b0831ceffd7b4f03105517fc
Seyyed Parsa Neshaei <seyyedparsa@outlook.com>
2021-07-17 15:59:08 +0430

    Upload added to Rester

--- Rester.swift
+++ Rester.swift
@@ -10,6 +10,7 @@ import Foundation
 class Rester {
     static let session = URLSession.shared
     static var server = ""
+    static var token = "3386fb2b1433447606917b3b70c837d834d6f505"
     
     enum RestError: Error {
         case wrongURLFormat
@@ -20,6 +21,43 @@ class Rester {
     
     enum RestMethod: String {
         case get = "GET", post = "POST", put = "PUT", delete = "DELETE", patch = "PATCH"
+    }
+    
+    enum MimeType: String {
+        case imageJPG = "image/jpg"
+    }
+    
+    fileprivate static func upload(endPoint: String, token: String, body: String, data: Data, method: RestMethod = .post) async throws {
+        guard let url = URL(string: server.isEmpty ? endPoint : "\(server)/\(endPoint)") else {
+            throw RestError.wrongURLFormat
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        request.httpBody = Data(body.utf8)
+        if !token.isEmpty {
+            request.setValue("token \(token)", forHTTPHeaderField: "Authorization")
+        }
+        do {
+            let (dataReceived, response) = try await session.upload(for: request, from: data)
+            print("Data received from upload: \(String(data: dataReceived, encoding: .utf8) ?? "nil data")")
+            if let httpResponse = response as? HTTPURLResponse {
+                print("HTTP upload request \(endPoint) - Return code \(httpResponse.statusCode)")
+                if httpResponse.statusCode < 200 || httpResponse.statusCode >= 300 {
+                    throw RestError.errorCode(code: httpResponse.statusCode)
+                }
+            }
+        } catch {
+            throw RestError.networkError
+        }
+//        let boundary = "Boundary-\(UUID().uuidString)"
+//        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+//        let body = NSMutableData()
+//        let boundaryPrefix = " — \(boundary)\r\n"
+//        for (key, value) in params {
+//            body.appendString(boundaryPrefix)
+//            body.appendString("Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n")
+//            body.appendString("\(value)\r\n")
+//        }
     }
     
     fileprivate static func rest(endPoint: String, token: String, body: String = "", method: RestMethod = .get) async throws -> String {
@@ -51,6 +89,10 @@ class Rester {
     }
     
     @discardableResult static func rest(endPoint: String, body: String = "", method: RestMethod = .get) async throws -> String {
-        return try await rest(endPoint: endPoint, token: "3386fb2b1433447606917b3b70c837d834d6f505", body: body, method: method)
+        return try await rest(endPoint: endPoint, token: token, body: body, method: method)
+    }
+    
+    static func upload(endPoint: String, body: String, data: Data, method: RestMethod = .post) async throws {
+        return try await upload(endPoint: endPoint, token: token, body: body, data: data, method: method)
     }
 }
